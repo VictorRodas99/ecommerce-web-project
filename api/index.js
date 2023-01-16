32f20843ae255a04524cb506859247178011778c
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-15 22:07:05 -0300

    add new endpoint

--- api/index.js
+++ api/index.js
@@ -9,12 +9,36 @@ app.get('/', (ctx) => {
 			endpoint: '/products',
 			description: 'Returns the current list of products',
 			method: 'GET'
+		},
+		{
+			endpoint: '/products/:id',
+			description: 'Returns product by id',
+			method: 'GET',
+			parameters: {
+				id: 'number'
+			}
 		}
 	])
 })
 
 app.get('/products', (ctx) => {
 	return ctx.json(products)
+})
+
+app.get('/products/:id', (ctx) => {
+	const id = Number(ctx.req.param('id'))
+
+	if(isNaN(id)) {
+		return ctx.json({
+			error: 'id parameter must be a number!'
+		}, 400)
+	}
+
+	const foundProduct = products.find(product => product.id === id)
+
+	return foundProduct 
+			? ctx.json(foundProduct)
+			: ctx.json({ message: `Product with id ${id} not found!` }, 404)
 })
 
 export default app
