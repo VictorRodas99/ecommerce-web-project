93f1a54ee6ca33d9f91b86a95989d687ff2c43e9
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-16 17:16:13 -0300

    format code

--- api/controllers/info.controller.js
+++ api/controllers/info.controller.js
@@ -1,17 +1,17 @@
 export const infoEndpoints = (ctx) => {
-    return ctx.json([
-		{
-			endpoint: '/products',
-			description: 'Returns the current list of products',
-			method: 'GET'
-		},
-		{
-			endpoint: '/products/:id',
-			description: 'Returns product by id',
-			method: 'GET',
-			parameters: {
-				id: 'number'
-			}
-		}
-	])
+  return ctx.json([
+    {
+      endpoint: '/products',
+      description: 'Returns the current list of products',
+      method: 'GET'
+    },
+    {
+      endpoint: '/products/:id',
+      description: 'Returns product by id',
+      method: 'GET',
+      parameters: {
+        id: 'number'
+      }
+    }
+  ])
 }
--- api/controllers/products.controller.js
+++ api/controllers/products.controller.js
@@ -1,21 +1,24 @@
 import products from '../../db/products.json'
 
 export const getProducts = (ctx) => {
-    return ctx.json(products)
+  return ctx.json(products)
 }
 
 export const getProductsById = (ctx) => {
-	const id = Number(ctx.req.param('id'))
+  const id = Number(ctx.req.param('id'))
 
-	if(isNaN(id)) {
-		return ctx.json({
-			error: 'id parameter must be a number!'
-		}, 400)
-	}
+  if (isNaN(id)) {
+    return ctx.json(
+      {
+        error: 'id parameter must be a number!'
+      },
+      400
+    )
+  }
 
-	const foundProduct = products.find(product => product.id === id)
+  const foundProduct = products.find((product) => product.id === id)
 
-	return foundProduct 
-			? ctx.json(foundProduct)
-			: ctx.json({ message: `Product with id ${id} not found!` }, 404)
+  return foundProduct
+    ? ctx.json(foundProduct)
+    : ctx.json({ message: `Product with id ${id} not found!` }, 404)
 }
