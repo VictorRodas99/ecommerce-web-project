818672bcf821aaf0192aab94ea12936248f9688b
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-17 00:47:16 -0300

    add urls to scrap

---
+++ scraping/scraper.config.js
@@ -0,0 +1,33 @@
+export const URLS = {
+  computing: {
+    home: 'https://nissei.com/py/informatica?product_list_limit=40',
+    notebooks:
+      'https://nissei.com/py/informatica/notebooks?product_list_limit=40',
+    printers:
+      'https://nissei.com/py/informatica/impresoras-y-suplementos?product_list_limit=40',
+    monitors:
+      'https://nissei.com/py/informatica/accesorios-y-componentes/monitores?product_list_limit=40',
+    storage:
+      'https://nissei.com/py/informatica/almacenamiento?product_list_limit=40',
+    internet:
+      'https://nissei.com/py/informatica/redes-e-internet?product_list_limit=40'
+  },
+  components: {
+    processors:
+      'https://nissei.com/py/informatica/accesorios-y-componentes/procesadores-cpu?product_list_limit=40',
+    motherboards:
+      'https://nissei.com/py/informatica/accesorios-y-componentes/placas-madres?product_list_limit=40',
+    ram: 'https://nissei.com/py/informatica/accesorios-y-componentes/memorias-ram?product_list_limit=40',
+    graphicsCards:
+      'https://nissei.com/py/informatica/accesorios-y-componentes/tarjetas-graficas?product_list_limit=40',
+    keyboards:
+      'https://nissei.com/py/informatica/accesorios-y-componentes/teclados?product_list_limit=40'
+  },
+  electronics: {
+    smartphones:
+      'https://nissei.com/py/electronica/celulares-tabletas/celulares-accesorios/telefonos-inteligentes?cat=140%2C285%2C753%2C286&product_list_limit=40',
+    televisions:
+      'https://nissei.com/py/electronica/televisores-home-theaters?product_list_limit=40',
+    audio: 'https://nissei.com/py/audio?product_list_limit=40'
+  }
+}
