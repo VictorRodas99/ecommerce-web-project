169560886b9af1f996c08b52f17b9e0d76da7967
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-18 00:13:37 -0300

    create data file to avoid several imports

---
+++ api/data/data-products.js
@@ -0,0 +1,31 @@
+import graphicsCards from '../../db/graphicsCards.json'
+import homeProducts from '../../db/home-products.json'
+import motherboards from '../../db/motherboards.json'
+import smartphones from '../../db/smartphones.json'
+import televisions from '../../db/televisions.json'
+import internetData from '../../db/internet.json'
+import processors from '../../db/processors.json'
+import notebooks from '../../db/notebooks.json'
+import storageData from '../../db/storage.json'
+import keyboards from '../../db/keyboards.json'
+import audioDevices from '../../db/audio.json'
+import printers from '../../db/printers.json'
+import monitors from '../../db/monitors.json'
+import productsRAM from '../../db/ram.json'
+
+export const data = {
+  graphicsCards,
+  homeProducts,
+  motherboards,
+  smartphones,
+  televisions,
+  internetData,
+  processors,
+  notebooks,
+  storageData,
+  keyboards,
+  audioDevices,
+  printers,
+  monitors,
+  productsRAM
+}
