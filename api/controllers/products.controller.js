33f9f4b26dd950e0c47af4dd58477445a852b574
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-18 17:41:33 -0300

    update getProductById

--- api/controllers/products.controller.js
+++ api/controllers/products.controller.js
@@ -1,20 +1,4 @@
-import { data } from '../data/data-products.js'
-
 export const getProductsById = (ctx) => {
-  const id = Number(ctx.req.param('id'))
-
-  if (isNaN(id)) {
-    return ctx.json(
-      {
-        error: 'id parameter must be a number!'
-      },
-      400
-    )
-  }
-
-  const foundProduct = data.homeProducts.find((product) => product.id === id)
-
-  return foundProduct
-    ? ctx.json(foundProduct)
-    : ctx.json({ message: `Product with id ${id} not found!` }, 404)
+  const { result, statusCode } = ctx.req.response
+  return ctx.json(result, statusCode)
 }
