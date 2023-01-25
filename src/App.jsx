4c7b80cbe59bf1e3d4ce604ba152fdb8209d83e3
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-25 12:12:31 -0300

    add basic styles for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import './App.css'
+import { MdStorefront, MdLocalMall, MdSearch } from 'react-icons/md'
+import './css/header.css'
 
 function App() {
-  return <h1>Setup React</h1>
+  return (
+    <header className="header-container">
+      <nav>
+        <div className="home-icon-container">
+          <a href="/">
+            <MdStorefront className="icon home-icon" />
+          </a>
+        </div>
+
+        <form className="form-container">
+          <div className="search-container">
+            <div className="search-input">
+              <MdSearch className="search-icon" />
+              <input placeholder="Busca aquí..." />
+            </div>
+            <button type="submit" className="btn-submit">
+              Buscar
+            </button>
+          </div>
+        </form>
+
+        <div className="cart-logo-container">
+          <div className="cart-icon">
+            <MdLocalMall className="icon" />
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
 }
 
 export default App
