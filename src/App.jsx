29db59ecd1c1ed137500aec4a712695eb5c29a0f
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-25 17:18:21 -0300

    extract header in component + add new section

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
 import './App.css'
-import { MdStorefront, MdLocalMall, MdSearch } from 'react-icons/md'
-import './css/header.css'
+import { Header } from './components/Header'
 
 function App() {
   return (
-    <header className="header-container">
-      <nav>
-        <div className="home-icon-container">
-          <a href="/">
-            <MdStorefront className="icon home-icon" />
-          </a>
-        </div>
+    <>
+      <Header />
+      <main>
+        <section className="categories">
+          <div className="menu-categories"></div>
 
-        <form className="form-container">
-          <div className="search-container">
-            <div className="search-input">
-              <MdSearch className="search-icon" />
-              <input placeholder="Busca aquí..." />
-            </div>
-            <button type="submit" className="btn-submit">
-              Buscar
-            </button>
-          </div>
-        </form>
-
-        <div className="cart-logo-container">
-          <div className="cart-icon">
-            <MdLocalMall className="icon" />
-          </div>
-        </div>
-      </nav>
-    </header>
+          <div className="top-categories"></div>
+        </section>
+        <section className="products-container"></section>
+      </main>
+    </>
   )
 }
 
