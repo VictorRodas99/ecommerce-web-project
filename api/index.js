c4eb41416351c4a90689b142d5abf6c714983055
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-27 16:33:16 -0300

    add cors middleware

--- api/index.js
+++ api/index.js
@@ -6,10 +6,12 @@ import { optionalSlash } from './middleware/trailing-slashes'
 import hardwareRoutes from './routes/hardware.js'
 import home from './routes/home.js'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
 /* Middlewares */
+app.use('*', cors({ origin: '*' }))
 app.use('*', catchProduct)
 
 /* Endpoints */
