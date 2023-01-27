8e68cb27b633c59fe337d4f74bf28f5fe15e967a
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-27 14:13:39 -0300

    add absolute paths

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import jsconfigPaths from 'vite-jsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), jsconfigPaths()],
   server: {
     port: 8000
   }
