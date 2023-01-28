880b728ef22dcc7bcbbe80327ed2577e3949f1c1
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-28 19:27:21 -0300

    add controller to get paginated products json

--- api/controllers/products.controller.js
+++ api/controllers/products.controller.js
@@ -1,3 +1,7 @@
+export const getData = (ctx) => {
+  return ctx.json(ctx.res.paginated)
+}
+
 export const getProductsById = (ctx) => {
   const { result, statusCode } = ctx.req.response
   return ctx.json(result, statusCode)
