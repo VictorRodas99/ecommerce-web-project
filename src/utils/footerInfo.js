64293f60af626047d34e58c50eb61b980d9bcbe2
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-31 17:32:55 -0300

    add footer info

---
+++ src/utils/footerInfo.js
@@ -0,0 +1,10 @@
+export const footerInfo = {
+  author: {
+    email: 'victormrodas99@gmail.com',
+    instagram: 'https://instagram.com/dy1ng_99?igshid=ZDdkNTZiNTM=',
+    github: 'https://github.com/VictorRodas99'
+  },
+  project: {
+    repo: 'https://github.com/VictorRodas99/ecommerce-web-project'
+  }
+}
