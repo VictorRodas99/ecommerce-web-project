b99a96d21dc496fb77dce9b4aa713676ea6fb1fb
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-31 13:07:09 -0300

    refactor code using products hook + fix pages bug

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,43 +1,27 @@
 import '@css/products.css'
-import { useState, useEffect } from 'react'
-import { getProducts } from '@services/getProducts.js'
 import { ProductCard } from './products/ProductCard'
 import { MdArrowBack, MdArrowForward } from 'react-icons/md'
 import { Arrow } from './products/Arrow'
+import { useProducts } from '@hooks/useProducts'
+
+const API_URL_HOME =
+  'https://ecommerce-products-api.vik-apps.workers.dev/products?page=1'
+const API_URL = 'https://ecommerce-products-api.vik-apps.workers.dev'
 
 export function Products() {
-  const [products, setProducts] = useState([])
-  const [previousPage, setPreviuosPage] = useState(undefined)
-  const [nextPage, setNextPage] = useState(undefined)
-
-  useEffect(() => {
-    getProducts(
-      'https://ecommerce-products-api.vik-apps.workers.dev/products?page=1'
-    ).then((page) => {
-      const { data: products, previousPage, nextPage } = page
-
-      setProducts(products)
-      setPreviuosPage(previousPage)
-      setNextPage(nextPage)
-    })
-  }, [])
+  const { products, pages, refreshProducts } = useProducts({
+    apiUrl: API_URL_HOME
+  })
 
   const changePage = (event) => {
-    const { id } = event.target
-    const numberPage = id === 'back' ? previousPage : nextPage
+    const { id } = event.currentTarget
+    const numberPage = id === 'back' ? pages.previousPage : pages.nextPage
+    const newPageUrl = `${API_URL}/products?page=${numberPage}`
 
-    getProducts(
-      `https://ecommerce-products-api.vik-apps.workers.dev/products?page=${numberPage}`
-    ).then((page) => {
-      const {
-        data: products,
-        previousPage: newPreviousPage,
-        nextPage: newNextPage
-      } = page
+    console.log(newPageUrl)
 
-      setProducts(products)
-      setPreviuosPage(newPreviousPage)
-      setNextPage(newNextPage)
+    refreshProducts({
+      apiUrl: newPageUrl
     })
   }
 
@@ -56,14 +40,14 @@ export function Products() {
       <div className="page-controller">
         <Arrow
           id="back"
-          page={previousPage}
+          page={pages.previousPage}
           icon={<MdArrowBack className="icon" />}
           eventHandler={changePage}
         />
 
         <Arrow
           id="next"
-          page={nextPage}
+          page={pages.nextPage}
           icon={<MdArrowForward className="icon" />}
           eventHandler={changePage}
         />
