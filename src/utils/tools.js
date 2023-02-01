63e4bc903577f2ec90e55a8951544443a356a782
Victor Rodas <victormatiasrodas100@gmail.com>
2023-01-31 22:35:32 -0300

    add tools

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -1,3 +1,11 @@
 export const capitalize = (string) => {
   return string[0].toUpperCase() + string.substring(1)
 }
+
+export const randomNumber = () => {
+  return Math.floor(Math.random() * 255)
+}
+
+export const getColorFormat = (color) => {
+  return `rgb(${color.red}, ${color.green}, ${color.blue})`
+}
