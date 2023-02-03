4bb2c6b3f97c348ef4593cbd28cf4f3d1e4581e0
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-03 17:13:55 -0300

    add shadow block + add product info inside cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,9 @@
-import { MdClose, MdShoppingBag, MdRemoveShoppingCart } from 'react-icons/md'
+import {
+  MdClose,
+  MdDelete,
+  MdShoppingBag,
+  MdRemoveShoppingCart
+} from 'react-icons/md'
 import { useContext } from 'react'
 import { CartContext } from '@context/CartContext'
 import '@css/cart.css'
@@ -7,43 +12,78 @@ export function Cart() {
   const { cartProducts, deleteProduct, cartVisibility, modifyCartVisibility } =
     useContext(CartContext)
 
+  const totalProducts = cartProducts.length
   const displayMode = cartVisibility ? 'cart-visible' : 'cart-invisible'
+  const amountDescription = `${totalProducts} ${
+    totalProducts === 1 ? ' Item' : ' Items'
+  }`
 
   return (
-    <aside className={`cart-container ${displayMode}`}>
-      <div className="cart-header">
-        <MdShoppingBag />
-        <button
-          className="cart-close"
-          onClick={() => modifyCartVisibility(false)}
-        >
-          <MdClose />
-        </button>
-      </div>
+    <>
+      <div
+        className="shadow-block"
+        style={{ display: cartVisibility ? 'block' : 'none' }}
+      ></div>
+      <aside className={`cart-container ${displayMode}`}>
+        <div className="cart-header">
+          <div className="cart-amount">
+            <MdShoppingBag />
+            {Boolean(totalProducts) && <p>{amountDescription}</p>}
+          </div>
 
-      <div className="cart-body">
-        <div
-          className="message-empty-cart"
-          style={{ display: cartProducts.length === 0 ? 'grid' : 'none' }}
-        >
-          <div className="message-content">
-            <MdRemoveShoppingCart />
-            <p>Tu carrito está vacío</p>
-            <p>Empieza a comprar</p>
+          <button
+            className="cart-close"
+            onClick={() => modifyCartVisibility(false)}
+          >
+            <MdClose />
+          </button>
+        </div>
+
+        <div className="cart-body">
+          <div
+            className="message-empty-cart"
+            style={{ display: totalProducts === 0 ? 'grid' : 'none' }}
+          >
+            <div className="message-content">
+              <MdRemoveShoppingCart />
+              <p>Tu carrito está vacío</p>
+              <p>Empieza a comprar</p>
+            </div>
+          </div>
+
+          <div className="cart-products">
+            {cartProducts.map((product) => {
+              const { name, price, image } = product
+              return (
+                <div className="cart-product-card" key={name}>
+                  <div className="product-cart-image">
+                    <img src={image} alt="Producto del carrito" />
+                  </div>
+                  <div className="product-cart-details">
+                    <h4>
+                      {name} <span className="dots">...</span>
+                    </h4>
+                    <p className="product-cart-price">{price}</p>
+                  </div>
+                  <div
+                    className="delete-icon"
+                    onClick={() => {
+                      deleteProduct(product)
+                    }}
+                  >
+                    <MdDelete />
+                  </div>
+                </div>
+              )
+            })}
           </div>
         </div>
 
-        <div className="cart-products">
-          {cartProducts.map((product) => (
-            <div className="cart-product-card">{JSON.stringify(product)}</div>
-          ))}
-        </div>
-      </div>
-
-      <div
-        className="cart-footer"
-        style={{ display: cartProducts.length > 0 ? 'block' : 'none' }}
-      ></div>
-    </aside>
+        <div
+          className="cart-footer"
+          style={{ display: cartProducts.length > 0 ? 'block' : 'none' }}
+        ></div>
+      </aside>
+    </>
   )
 }
