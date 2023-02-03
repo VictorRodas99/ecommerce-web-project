d8efd73310fe215fcb16679516dcbc7d96484df8
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-03 20:22:08 -0300

    add new tools to format price

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -9,3 +9,21 @@ export const randomNumber = () => {
 export const getColorFormat = (color) => {
   return `rgb(${color.red}, ${color.green}, ${color.blue})`
 }
+
+export const pricesToNumber = (products) => {
+  const prices = products.map((product) => {
+    const parsedPrice = Number(product.price.replace(/\D/g, ''))
+    return isNaN(parsedPrice) ? 0 : parsedPrice
+  })
+
+  return prices
+}
+
+export const getTotalPriceOf = (products) => {
+  const parsedPrices = pricesToNumber(products)
+  const totalPrice = parsedPrices.reduce(
+    (accmulator, currentPrice) => accmulator + currentPrice
+  )
+
+  return `Gs. ${totalPrice.toLocaleString('de-DE')}`
+}
