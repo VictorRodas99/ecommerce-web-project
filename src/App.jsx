5778f063843d598bf020c88a8947072a784a9f85
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-02 22:49:28 -0300

    add cart component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { Header } from '@components/Header'
 import { Footer } from '@components/Footer'
 import { Categories } from '@components/Categories'
 import { Products } from '@components/Products'
+import { Cart } from '@components/Cart'
 
 function App() {
   return (
     <>
       <Header />
+      <Cart />
       <main>
         <Categories />
         <Products />
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,1 +1,41 @@
-export function Cart() {}
+import { MdClose, MdShoppingBag, MdRemoveShoppingCart } from 'react-icons/md'
+import { useContext } from 'react'
+import { CartContext } from '@context/CartContext'
+import '@css/cart.css'
+
+export function Cart() {
+  const { cartProducts, deleteProduct } = useContext(CartContext)
+
+  return (
+    <aside className="cart-container" style={{ display: 'none' }}>
+      <div className="cart-header">
+        <MdShoppingBag />
+        <MdClose />
+      </div>
+
+      <div className="cart-body">
+        <div
+          className="message-empty-cart"
+          style={{ display: cartProducts.length === 0 ? 'grid' : 'none' }}
+        >
+          <div className="message-content">
+            <MdRemoveShoppingCart />
+            <p>Tu carrito está vacío</p>
+            <p>Empieza a comprar</p>
+          </div>
+        </div>
+
+        <div className="cart-products">
+          {cartProducts.map((product) => (
+            <div className="cart-product-card">{JSON.stringify(product)}</div>
+          ))}
+        </div>
+      </div>
+
+      <div
+        className="cart-footer"
+        style={{ display: cartProducts.length > 0 ? 'block' : 'none' }}
+      ></div>
+    </aside>
+  )
+}
