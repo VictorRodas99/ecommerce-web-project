daf7693c912aa475ae78d94d14830f14a7c85dc5
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-03 17:06:43 -0300

    add effect to stop scroll when cart is visible

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,22 @@ import { Footer } from '@components/Footer'
 import { Categories } from '@components/Categories'
 import { Products } from '@components/Products'
 import { Cart } from '@components/Cart'
+import { CartContext } from '@context/CartContext'
+import { useContext, useEffect } from 'react'
 
 function App() {
+  const { cartVisibility } = useContext(CartContext)
+
+  useEffect(() => {
+    if (cartVisibility) {
+      document.body.classList.add('stop-scroll')
+    }
+
+    return () => {
+      document.body.classList.remove('stop-scroll')
+    }
+  }, [cartVisibility])
+
   return (
     <>
       <Header />
