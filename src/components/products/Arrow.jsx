61a079f90353cc331bf67b9bfd2f6317fc3bde2d
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-23 11:18:41 -0300

    fix handler function name

--- src/components/products/Arrow.jsx
+++ src/components/products/Arrow.jsx
@@ -1,11 +1,11 @@
 export function Arrow({ id, page, icon, eventHandler }) {
-  const handeClick = (event) => eventHandler(event)
+  const handleClick = (event) => eventHandler(event)
 
   return (
     <div
       id={id}
       className={page ? 'arrow-container' : `arrow-disabled`}
-      onClick={handeClick}
+      onClick={handleClick}
     >
       <div className="page-message">{page ? `Pág. ${page}` : '🛇'}</div>
       {icon}
