501dc43ecc59fe579173349222049a422cd58071
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-23 18:08:49 -0300

    install plugin for vite to compress in-build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import jsconfigPaths from 'vite-jsconfig-paths'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
-  plugins: [react(), jsconfigPaths()],
+  plugins: [react(), jsconfigPaths(), viteCompression()],
   server: {
     port: 8000
   }
