c1d67ed855a8e10c524188f66b05ecb6b25bce5d
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-25 00:46:23 -0300

    refactor code using new hook for events + use notification events using context

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -1,76 +1,62 @@
-import { useState } from 'react'
-import { MdAddShoppingCart, MdDone, MdClose } from 'react-icons/md'
 import { Ring } from '@uiball/loaders'
-import { useEffect } from 'react'
-import { useCart } from '@hooks/useCart'
 import { Image } from '@components/Image'
+import { useProductCard } from '@hooks/useProductCard'
+import { useCart } from '@hooks/useCart'
+import { notificationIcons } from '@components/icons/NotificationIcons'
+import { useEffect } from 'react'
+import { useNotification } from '@hooks/useNotification'
 
-export function ProductCard({ data, notificationEvent }) {
+export function ProductCard({ data }) {
+  const {
+    CardIcon,
+    productWasAdded,
+    changeIconInAdd,
+    changeIconInDelete,
+    imageIsLoading,
+    handleImageLoad
+  } = useProductCard()
+
   const { cartProducts, addProduct } = useCart()
-  const [productWasAdded, setProductWasAdded] = useState(false)
-  const [loading, setLoading] = useState(true)
-  const [clicked, setClicked] = useState(false)
-  const [iconCart, setIconCart] = useState(undefined)
+  const { createNotification } = useNotification()
 
-  const { id, name, price } = data
-  const image = data.srcImages[0]
+  const handleClickOnCart = () => {
+    addProduct({
+      ...data,
+      image: data.srcImages[1] ?? data.srcImages[0]
+    })
+
+    changeIconInAdd()
+
+    createNotification({
+      color: 'success',
+      message: 'Agregado a carrito!',
+      icon: notificationIcons.done
+    })
+  }
 
   useEffect(() => {
     if (productWasAdded) {
-      // verificar si ya no existe el id de este producto de esta card
       const existsCurrentProduct = cartProducts.some(
-        (product) => product.id === id
+        (product) => product.id === data.id
       )
 
       if (!existsCurrentProduct) {
-        setClicked(false)
-        setProductWasAdded(false)
-        changeIcon(iconCart)
-
-        notificationEvent({
-          color: 'error',
-          message: 'Eliminado del carrito!',
-          icon: <MdClose className="icon" />
-        })
+        changeIconInDelete()
       }
     }
   }, [cartProducts])
 
-  const handleLoad = () => {
-    setLoading(false)
-  }
-
-  const changeIcon = (target) => {
-    const currentIcon = target
-    const checkIcon = target.nextElementSibling
-
-    currentIcon.classList.toggle('visible-icon')
-    checkIcon.classList.toggle('visible-icon')
-  }
-
-  const handleClickOnCart = ({ currentTarget }) => {
-    setIconCart(currentTarget)
-    setClicked(true)
-    addProduct({
-      id,
-      name,
-      price,
-      image: data.srcImages[1] ?? data.srcImages[0]
-    })
-    notificationEvent({
-      color: 'success',
-      message: 'Agregado a carrito!',
-      icon: <MdDone className="icon" />
-    })
-    changeIcon(currentTarget)
-    setProductWasAdded(true)
-  }
+  const { name, price } = data
+  const image = data.srcImages[0]
 
   return (
     <div className="product-card">
       <div className="card-header">
-        <Image src={image} alt={name} events={{ onLoad: handleLoad }} />
-        <div className="loader" style={{ display: loading ? 'flex' : 'none' }}>
+        <Image src={image} alt={name} events={{ onLoad: handleImageLoad }} />
+        <div
+          className="loader"
+          style={{ display: imageIsLoading ? 'flex' : 'none' }}
+        >
           <Ring size={70} lineWeight={3} speed={2} color="#7c828d2a" />
         </div>
       </div>
@@ -81,13 +67,10 @@ export function ProductCard({ data, notificationEvent }) {
         <p className="price">{price}</p>
         <div className="add-icon">
           <div
-            className="product-card-icon visible-icon"
-            onClick={!clicked ? handleClickOnCart : null}
+            className="product-card-icon"
+            onClick={!productWasAdded ? handleClickOnCart : null}
           >
-            <MdAddShoppingCart className="icon" />
-          </div>
-          <div className="product-card-icon">
-            <MdDone className="icon" />
+            <CardIcon className="icon" />
           </div>
         </div>
       </div>
