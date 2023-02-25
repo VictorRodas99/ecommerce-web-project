41f384bdd622c8365732572c24e10a07445da455
Victor Rodas <victormatiasrodas100@gmail.com>
2023-02-25 00:48:46 -0300

    use notification events with context

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,8 +1,11 @@
 import { MdClose } from 'react-icons/md'
 import { useCart } from '@hooks/useCart'
+import { useNotification } from '@hooks/useNotification'
+import { notificationIcons } from '@components/icons/NotificationIcons'
 
 export function CartItem({ data: product }) {
   const { deleteProduct } = useCart()
+  const { createNotification } = useNotification()
   const { name, price, image } = product
 
   return (
@@ -20,6 +23,11 @@ export function CartItem({ data: product }) {
         className="delete-icon"
         onClick={() => {
           deleteProduct(product)
+          createNotification({
+            color: 'error',
+            message: 'Eliminado del carrito!',
+            icon: notificationIcons.deleted
+          })
         }}
       >
         <MdClose />
