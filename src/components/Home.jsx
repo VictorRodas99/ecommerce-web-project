05b523ff06109cb4748a5adf7524625a8cfacd4b
Victor Rodas <victormatiasrodas100@gmail.com>
2023-03-02 12:53:52 -0300

    create a component that contains all the components that need to be rendered only on home

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import { Categories } from '@components/Categories'
+import { Products } from '@components/Products'
+
+export function Home() {
+  return (
+    <>
+      <Categories />
+      <Products />
+    </>
+  )
+}
