99713aa43cd61b5b164c10f4780d74314e8908ff
Victor Rodas <victormatiasrodas100@gmail.com>
2023-03-02 18:01:28 -0300

    add functions to avoid unnecessary fecthing

--- src/services/getProducts.js
+++ src/services/getProducts.js
@@ -1,8 +1,33 @@
+const saveProductsInStorage = ({ url, data }) => {
+  const key = window.btoa(url) // Encondes in base64 the url to use it as a key
+
+  try {
+    window.sessionStorage.setItem(key, JSON.stringify(data))
+  } catch (error) {
+    console.error(
+      `An error has ocurred while trying to save data in sessionStorage: ${error.message}`
+    )
+  }
+}
+
+const getSavedProductInStorage = (url) => {
+  const key = window.btoa(url)
+  const data = window.sessionStorage.getItem(key)
+
+  return JSON.parse(data)
+}
+
 export const getProducts = async (url) => {
+  const savedData = getSavedProductInStorage(url)
+
+  if (savedData) return savedData
+
   const response = await fetch(url)
 
   if (!response.ok) return []
 
   const data = await response.json()
+  saveProductsInStorage({ url, data })
+
   return data
 }
