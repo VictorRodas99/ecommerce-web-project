da2fc1f183aabd56e55c7cfc774af17e60cb94f7
Victor Rodas <victormatiasrodas100@gmail.com>
2023-03-05 22:28:59 -0300

    add toggleClass and scroll function + add number parser

--- src/utils/tools.js
+++ src/utils/tools.js
@@ -10,6 +10,11 @@ export const getColorFormat = (color) => {
   return `rgb(${color.red}, ${color.green}, ${color.blue})`
 }
 
+export const parseNumber = (number) => {
+  const parsedNumber = Number(number.replace(/\D/g, ''))
+  return isNaN(parsedNumber) ? 0 : parsedNumber
+}
+
 export const pricesToNumber = (products) => {
   const prices = products.map((product) => {
     const parsedPrice = Number(product.price.replace(/\D/g, ''))
@@ -30,4 +35,67 @@ export const getTotalPriceOf = (products) => {
 
 export const parseNameToURI = (givenName) => {
   return encodeURIComponent(givenName).replaceAll('%20', '-').toLowerCase()
+}
+
+/**
+ * @param {{
+ *  element: HTMLElement,
+ *  classes: { add: string, remove: string }
+ *  }}
+ */
+export const toggleClass = ({ element, classes }) => {
+  // bc classList.toggle doesn't work for some reason...
+  element.classList.add(classes.add)
+  element.classList.remove(classes.remove)
+}
+
+/**
+ *
+ * @param {{
+ *   container: HTMLElement,
+ *   condition: boolean,
+ *   relativeTo?: HTMLElement,
+ *   pxToScroll?: number,
+ *   timesToScroll?: number
+ * }}
+ */
+export const simulateScroll = ({
+  container,
+  condition,
+  relativeTo = undefined,
+  pxToScroll = 0,
+  timesToScroll = 0
+}) => {
+  if (isNaN(pxToScroll)) {
+    throw new Error('pixels to scroll must be only a number')
+  }
+
+  if (!pxToScroll && !relativeTo) {
+    throw new Error('Provide at least one method of scrolling')
+  }
+
+  const TABLET_SCREEN_WIDTH_PX = 768
+  const isDesktopScreen = window.innerWidth > TABLET_SCREEN_WIDTH_PX
+  let scrollController
+
+  const scrollBevavior = isDesktopScreen ? 'scrollTop' : 'scrollLeft'
+  const offSetValue = isDesktopScreen ? 'offsetHeight' : 'offsetWidth'
+
+  if (pxToScroll) {
+    const containerGap = parseNumber(getComputedStyle(container).gap)
+    pxToScroll += containerGap
+    scrollController = pxToScroll
+  } else {
+    scrollController = relativeTo[offSetValue]
+  }
+
+  if (timesToScroll) {
+    scrollController *= timesToScroll
+  }
+
+  if (condition) {
+    container[scrollBevavior] = 0
+  } else {
+    container[scrollBevavior] = scrollController
+  }
 }
