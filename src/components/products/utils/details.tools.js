a42573a65106b9cecbd511ecc2fdec7800f77c47
Victor Rodas <victormatiasrodas100@gmail.com>
2023-03-05 22:29:50 -0300

    refactor adding gallery functions in separate files

--- src/components/products/utils/details.tools.js
+++ src/components/products/utils/details.tools.js
@@ -52,3 +52,34 @@ export const structureData = (rawData) => {
 export const getProductURLForAPI = ({ category, page }) => {
   return `${API_URLS.base}/${categoryForAPI[category]}?page=${page}`
 }
+
+export const getGalleryElements = (event) => {
+  const currentImageContainer = event.currentTarget
+  const gallery = currentImageContainer.parentElement
+  const firstImageInGallery = gallery.firstChild.firstChild
+  const lastImageInGallery = gallery.lastChild.firstChild
+  const mainImageContainer = gallery.nextElementSibling
+
+  const currentImage = event.target
+  const [mainImage] = mainImageContainer.children
+
+  return {
+    containers: {
+      currentImage: currentImageContainer,
+      mainImageContainer,
+      gallery
+    },
+    images: {
+      gallery: {
+        first: firstImageInGallery,
+        current: currentImage,
+        lastImage: lastImageInGallery
+      },
+      mainImage
+    }
+  }
+}
+
+export const getImageNumber = (gallery, imageContainer) => {
+  return Array.from(gallery.children).indexOf(imageContainer) + 1
+}
