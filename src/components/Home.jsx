98f6d7aa817409a6fb0e33b8c1dcf99d9cbb0bc9
Victor Rodas <victormatiasrodas100@gmail.com>
2023-03-11 17:47:09 -0300

    add api url externally to the Product component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import { Categories } from '@components/Categories'
 import { Products } from '@components/Products'
+import { API_URLS } from 'src/config'
 
 export function Home() {
   return (
     <>
       <Categories />
-      <Products />
+      <Products apiUrl={API_URLS.home} category="Productos" />
     </>
   )
 }
