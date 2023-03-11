f1e3b00044b53ced1231c0ffa806cb0318cdd578
Victor Rodas <victormatiasrodas100@gmail.com>
2023-03-11 17:48:34 -0300

    update config adding more urls

--- src/config.js
+++ src/config.js
@@ -1,4 +1,16 @@
+const BASE_URL = 'https://ecommerce-products-api.vik-apps.workers.dev'
+
 export const API_URLS = {
-  home: 'https://ecommerce-products-api.vik-apps.workers.dev/products?page=1',
-  base: 'https://ecommerce-products-api.vik-apps.workers.dev'
+  home: `${BASE_URL}/products`,
+  base: BASE_URL,
+  computing: {
+    notebooks: `${BASE_URL}/computing/notebooks`,
+    printers: `${BASE_URL}/computing/printers`,
+    monitors: `${BASE_URL}/computing/monitors`,
+    storage: `${BASE_URL}/computing/storage`,
+    internet: `${BASE_URL}/computing/internet-devices`
+  },
+  electronics: {
+    smartphones: `${BASE_URL}/electronics/smartphones`
+  }
 }
