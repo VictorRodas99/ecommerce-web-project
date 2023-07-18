64fb74516ac30cbd805f1d56afd9cb4dfec84644
Victor Rodas <victormatiasrodas100@gmail.com>
2023-07-18 14:34:56 -0400

    refactor Image component

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,16 +1,20 @@
-export function Image({ src, alt, role, priority, events, ...props }) {
-  const isBackground = role === 'presentation'
+/**
+ * @param {HTMLImageElement} imageProps
+ */
+export function Image({ children, ...props }) {
+  const existsLoadPriority = props.fetchPriority === 'high'
 
   return (
     <img
-      src={src}
-      loading={priority === 'high' ? 'lazy' : undefined}
-      decoding={priority === 'high' ? 'async' : 'auto'}
-      alt={alt}
-      role={isBackground ? 'presentation' : undefined}
-      fetchpriority={priority}
-      {...events}
+      src={props.src}
+      loading={existsLoadPriority ? 'lazy' : 'eager'}
+      decoding={existsLoadPriority ? 'async' : 'auto'}
+      alt={props.alt || 'Generic image'}
+      role="presentation"
+      fetchPriority={props.fetchPriority}
       {...props}
-    />
+    >
+      {children}
+    </img>
   )
 }
