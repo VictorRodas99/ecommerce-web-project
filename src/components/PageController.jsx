57eb00a54e03405372f8df14f5e2343d2704892e
Victor Rodas <victormatiasrodas100@gmail.com>
2023-07-19 12:16:52 -0400

    add corresponding handlers + refactor code

--- src/components/PageController.jsx
+++ src/components/PageController.jsx
@@ -1,21 +1,53 @@
 import { MdArrowBack, MdArrowForward } from 'react-icons/md'
 import { Arrow } from './products/Arrow'
+import { usePage } from '@hooks/usePage'
 
-export function PageController({ pages, handleChangePage }) {
+/**
+ * Renders a page controller component with arrows for navigating between pages
+ * @param {{
+ *  urlBase: string
+ *  totalPages: { previousPage?: number, nextPage?: number }
+ *  refreshMethod: (url: string) => void,
+ *  containerReference?: React.MutableRefObject<HTMLElement> | null,
+ * }} props
+ */
+export function PageController({
+  urlBase,
+  totalPages,
+  refreshMethod,
+  containerReference = null
+}) {
+  const { savePage } = usePage()
+
+  const changePage = (event) => {
+    const { id } = event.currentTarget
+    const numberPage =
+      id === 'back' ? totalPages.previousPage : totalPages.nextPage
+
+    const newPageUrl = `${urlBase}?page=${numberPage}`
+
+    savePage(numberPage)
+    refreshMethod(newPageUrl)
+
+    if (containerReference) {
+      window.scroll(0, containerReference.current.offsetTop)
+    }
+  }
+
   return (
     <div className="page-controller">
       <Arrow
         id="back"
-        page={pages.previousPage}
+        page={totalPages.previousPage}
         icon={<MdArrowBack className="icon" />}
-        eventHandler={handleChangePage}
+        eventHandler={changePage}
       />
 
       <Arrow
         id="next"
-        page={pages.nextPage}
+        page={totalPages.nextPage}
         icon={<MdArrowForward className="icon" />}
-        eventHandler={handleChangePage}
+        eventHandler={changePage}
       />
     </div>
   )
