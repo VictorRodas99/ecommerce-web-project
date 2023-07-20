2f982dea97854fbfad072368d3477377d6aedc81
Victor Rodas <victormatiasrodas100@gmail.com>
2023-07-20 15:41:20 -0400

    extract methods + document types

--- src/components/products/utils/details.tools.js
+++ src/components/products/utils/details.tools.js
@@ -9,6 +9,24 @@ export const categoryForAPI = {
   electronica: 'electronics'
 }
 
+export const refreshPage = () => {
+  document.title = 'Info-Shop | Producto'
+  window.scrollTo(0, 0)
+}
+
+/**
+ * @typedef {{
+ *  name: string,
+ *  price: string,
+ *  categories: string[],
+ *  details: string[],
+ *  images: string[],
+ *  mainImage: string,
+ *  wasAdded: boolean
+ * }} ProductDetail
+ * 
+ * @type {ProductDetail}
+ */
 export const productInitialState = {
   name: undefined,
   price: undefined,
@@ -46,35 +64,4 @@ export const structureData = (rawData) => {
 
 export const getProductURLForAPI = ({ category, page }) => {
   return `${API_URLS.base}/${categoryForAPI[category]}?page=${page}`
-}
-
-export const getGalleryElements = (event) => {
-  const currentImageContainer = event.currentTarget
-  const gallery = currentImageContainer.parentElement
-  const firstImageInGallery = gallery.firstChild.firstChild
-  const lastImageInGallery = gallery.lastChild.firstChild
-  const mainImageContainer = gallery.nextElementSibling
-
-  const currentImage = event.target
-  const [mainImage] = mainImageContainer.children
-
-  return {
-    containers: {
-      currentImage: currentImageContainer,
-      mainImageContainer,
-      gallery
-    },
-    images: {
-      gallery: {
-        first: firstImageInGallery,
-        current: currentImage,
-        lastImage: lastImageInGallery
-      },
-      mainImage
-    }
-  }
-}
-
-export const getImageNumber = (gallery, imageContainer) => {
-  return Array.from(gallery.children).indexOf(imageContainer) + 1
 }
