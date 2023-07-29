645dcc13ebf412036a3f5079eb395a6cd9964dfb
Victor Rodas <victormatiasrodas100@gmail.com>
2023-07-29 12:41:34 -0400

    add sorter context + save options in state

--- src/components/page-elements/Select.jsx
+++ src/components/page-elements/Select.jsx
@@ -3,9 +3,26 @@ import { SelectIcon, Options } from './select/Components'
 import { useSelect } from './hooks/useSelect'
 import { useEffect } from 'react'
 
+import { useSorters } from '@hooks/useSorters'
+
 export function Select({ label, options, valueSetter }) {
+  const sorterContext = useSorters() // returns undefined if is not wrapped on a Provider
   const { optionsVisibility, currentOption, value } = useSelect({ options })
-  useEffect(() => valueSetter(value), [value])
+
+  useEffect(() => {
+    if (sorterContext) {
+      const { saveSortingOptions } = sorterContext
+      saveSortingOptions(options)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (!value) {
+      return
+    }
+
+    valueSetter(value)
+  }, [value])
 
   return (
     <>
