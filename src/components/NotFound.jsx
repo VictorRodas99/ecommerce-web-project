aaaa5349817b362834cb4bd779ed5cae71a0513b
Victor Rodas <victormatiasrodas100@gmail.com>
2023-08-09 11:53:17 -0400

    fix blinking rendering

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,15 +1,24 @@
 import '@css/not-found.css'
+import { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 export default function NotFound() {
+  const [isUsed, setIsUsed] = useState(false)
   const navigate = useNavigate()
 
   const goBack = () => {
     navigate(-1)
   }
 
+  useEffect(() => {
+    setIsUsed(true)
+  }, [])
+
   return (
-    <section className="not-found-container">
+    <section
+      className="not-found-container"
+      style={{ display: isUsed ? 'grid' : 'none' }}
+    >
       <div className="not-found-messages">
         <h1>404...</h1>
         <h4>No se encontró la página que buscas</h4>
