62cf10ee0d53c3b1957bef891b73b17f303d3f58
Victor Rodas <victormatiasrodas100@gmail.com>
2023-08-08 22:04:17 -0400

    change items behavior + change styles

--- src/components/categories/CategoriesBox.jsx
+++ src/components/categories/CategoriesBox.jsx
@@ -1,19 +1,53 @@
-import { capitalize } from '@utils/tools.js'
+import { capitalize, parseNameToURI } from '@utils/tools.js'
+import { Link } from 'react-router-dom'
+
+function BoxBodyMainSection({ topics }) {
+  const topicsEntries = Object.entries(topics)
+
+  return (
+    <div className="box-body">
+      {topicsEntries.map(([topic, icon]) => (
+        <div className="box-body__item" key={topic}>
+          {icon}
+          {capitalize(topic)}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+function BoxBody({ topics }) {
+  const topicsEntries = Object.entries(topics)
+
+  return (
+    <div className="box-body">
+      {topicsEntries.map(([topic, icon]) => (
+        <Link
+          className="box-body__item"
+          key={topic}
+          to={`/category/${parseNameToURI(topic)}`}
+        >
+          {icon}
+          {capitalize(topic)}
+        </Link>
+      ))}
+    </div>
+  )
+}
 
 export function CategoriesBox({ title, topics }) {
+  const isCustom = title === 'Secciones'
+
   return (
     <div className="menu-box">
       <div className="box-title">
         <h3>{title}</h3>
       </div>
-      <div className="box-body">
-        {Object.entries(topics).map(([topic, icon]) => (
-          <div key={topic}>
-            {icon}
-            {capitalize(topic)}
-          </div>
-        ))}
-      </div>
+      {isCustom ? (
+        <BoxBodyMainSection topics={topics} />
+      ) : (
+        <BoxBody topics={topics} />
+      )}
     </div>
   )
 }
