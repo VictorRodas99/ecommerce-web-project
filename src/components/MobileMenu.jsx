a1bad5d9b17e0af8bbbd31684a44b4aac95b5865
Victor Rodas <victormatiasrodas100@gmail.com>
2023-08-14 17:21:48 -0400

    add interactivity in mobile menu

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -6,25 +6,39 @@ import {
   MdOutlineShoppingCart
 } from 'react-icons/md'
 
-export function MobileMenu() {
+import { Link } from 'react-router-dom'
+import { mobileSections } from './MobileComponents'
+
+/**
+ * @param {{ sectionController: ({ topic }: { topic: 'categories' | 'mainSections' | 'cart' }) => void }} props
+ */
+export default function MobileMenu({ sectionController }) {
   return (
     <section className="menu-mobile">
-      <button className="menu-mobile__item">
+      <Link className="menu-mobile__item menu-mobile-home" to="/">
         <MdHome />
         Home
-      </button>
-      <button className="menu-mobile__item">
+      </Link>
+      <button
+        className="menu-mobile__item"
+        onClick={() => sectionController({ topic: mobileSections.categories })}
+      >
         <MdViewComfy />
         Categorías
       </button>
-      <button className="menu-mobile__item">
+      <button
+        className="menu-mobile__item"
+        onClick={() =>
+          sectionController({ topic: mobileSections.mainSections })
+        }
+      >
         <MdDashboard />
         Secciones
       </button>
-      <button className="menu-mobile__item">
+      <Link className="menu-mobile__item menu-mobile-watch-cart" to="/carrito">
         <MdOutlineShoppingCart />
-        Ver Carrito
-      </button>
+        Carrito
+      </Link>
     </section>
   )
 }
