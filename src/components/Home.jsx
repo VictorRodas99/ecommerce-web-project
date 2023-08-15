dd996b86deed0f08be549c312b094e77fdb70e98
Victor Rodas <victormatiasrodas100@gmail.com>
2023-08-15 12:22:20 -0400

    fix title freeze

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,13 @@ import { Categories } from '@components/Categories'
 import { Products } from '@components/Products'
 import { API_URLS } from 'src/config'
 
+import { useEffect } from 'react'
+
 export function Home() {
+  useEffect(() => {
+    document.title = 'Info-Shop | Ecommerce'
+  }, [])
+
   return (
     <>
       <Categories />
